5725a5db1835d25fcf7c05b532cc74a03883bc88
Arthur Wang <rthrwang@gmail.com>
2017-07-27 10:42:28 +0800

    test.pl & smbols.json

--- test/test.pl
+++ test/test.pl
@@ -1,25 +1,8 @@
-rewrite_module_declaration(Module, PI) :-
-    F='/home/laowang/workspace/vsc-prolog/test/trytrace.pl',
-    setup_call_cleanup(
-        open(F, read, S),
-        (   read_term(S, Term, [term_position(Pos)]),
-            stream_position_data(line_count, Pos, Line),
-            stream_position_data(line_position, Pos, Start),
-            (   Term=(:-module(Module1, Exports))
-            ->  (   memberchk(PI, Exports)
-                ->  ReTerm=none,
-                    Action=none
-                ;   NewExp=[PI|Exports],
-                    ReTerm=(:-module(Module1, NewExp)),
-                    Action=replace
-                )
-            ;   ReTerm=(:-module(Module, [PI])),
-                Action=insert
-            ),
-            format('Action=~s;Mod=~w;Line=~d;Start=~d;~n',
-                [Action, ReTerm, Line, Start])
-        ),
-        close(S)
-    ).
-% start :-
-%     rewrite_module_declaration(ttrace, min_numlist/3).
+%%
+:- use_module(library(regex)).
+
+start(Str) :-
+    % regex('\\d+', [], Str, Captures),
+
+ Str =~ '\\d+',
+    format('~s contains digitals.~n', [Str]).
